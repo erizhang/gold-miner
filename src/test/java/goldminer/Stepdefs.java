7c31f6449a76cfaccd362ce6f98635607b5bbadf
liaoyuan.zly <liaoyuan.zly@alibaba-inc.com>
2019-10-10 15:19:38 +0800

    pass the first bdd case

--- src/test/java/goldminer/Stepdefs.java
+++ src/test/java/goldminer/Stepdefs.java
@@ -1,31 +1,36 @@
 package goldminer;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static org.junit.Assert.assertEquals;
 
 
+class IsItFriday{
+    static String isItFriday(String today) {
+        return "Nope";
+    }
+}
 
 public class Stepdefs {
 
+    private String today;
+    private String actualAnswer;
+
     @Given("^today is Sunday$")
-    public void today_is_Sunday() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void today_is_Sunday() {
+        today = "Sunday";
     }
 
     @When("^I ask whether it's Friday yet$")
-    public void i_ask_whether_it_s_Friday_yet() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
     }
 
     @Then("^I should be told \"([^\"]*)\"$")
-    public void i_should_be_told(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
     }
 
 }
