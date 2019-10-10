6de15b79e213c74d162ba780680c7e6a397029ea
liaoyuan.zly <liaoyuan.zly@alibaba-inc.com>
2019-10-10 15:07:37 +0800

    add the step definitions

---
+++ src/test/java/goldminer/Stepdefs.java
@@ -0,0 +1,31 @@
+package goldminer;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+
+
+public class Stepdefs {
+
+    @Given("^today is Sunday$")
+    public void today_is_Sunday() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I ask whether it's Friday yet$")
+    public void i_ask_whether_it_s_Friday_yet() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should be told \"([^\"]*)\"$")
+    public void i_should_be_told(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
